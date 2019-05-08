27500e79533dd107b1cfc7c535f828a52cd8e569
aashem <shemeikka.asla@gmail.com>
2019-05-08 12:25:00 +0300

    step4

--- puhkirja/src/App.js
+++ puhkirja/src/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react'
 
-const showPersons= (persons) =>{
-  const person = persons.map(person => Person(person.name, person.phone))
-  
-return(
-    person
+const showPersons= (persons, filter) =>{
+  let showPeople = persons
+  if ( filter ){
+    showPeople = persons.filter(person => person.name === filter)
+    console.log(showPeople, filter)
+  }
+    const retPeople = showPeople.map(person => Person(person.name, person.phone))
+    console.log(retPeople)
+  return(
+    retPeople
   )
 }
 
 const Person = (name, phone) => {
+
   return(
     <li>{name} {phone}</li>
   )
 }
 
 
+
+
 const App = () => {
-  const [ persons, setPersons] = useState([]) 
+
+  const [ persons, setPersons] = useState([ 
+  { name: 'Arto Hellas', number: '040-123456' },
+  { name: 'Martti Tienari', number: '040-123456' },
+  { name: 'Arto Järvinen', number: '040-123456' },
+  { name: 'Lea Kutvonen', number: '040-123456' }]) 
 
   const [ newName, setNewName ] = useState('')
-
   const [ newPhone, setNewPhone] = useState('')
+  const [ filter, setFilter] = useState('')
+  const [ newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -29,6 +43,17 @@ const App = () => {
   const handlePhoneChange = (event) => {
     setNewPhone(event.target.value)
   } 
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+
+  }
+
+  const newFilt = (event) => {
+    event.preventDefault()
+    setFilter(newFilter)
+    setNewFilter('')
+  }
 
   const newPerson = (event) => {
     event.preventDefault()
@@ -60,17 +85,23 @@ const App = () => {
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <form onSubmit={newFilt}> 
+      <div>
       <p>Rajaa Listaa</p>
-      Nimi: <input/>
+      Nimi: <input value={newFilter} onChange={handleFilterChange}/>
+      <button type ="submit">Rajaa</button>
+      </div>
+      </form>
+      <p>----------------------------</p>
       <form onSubmit={newPerson}>
         <div>
-          nimi: <input value={newName} onChange={handleNameChange}/>
+          Nimi: <input value={newName} onChange={handleNameChange}/>
           numero:<input value={newPhone} onChange={handlePhoneChange}/>
-          <button type="submit">lisää</button>
+          <button type="submit">Lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      <p>{showPersons(persons)}</p>
+      <p>{showPersons(persons, filter)}</p>
     </div>
   )
 
