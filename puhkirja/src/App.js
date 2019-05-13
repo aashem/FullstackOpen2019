c61c501e9e588713e09d4c2e780f635a7c7c7908
aashem <shemeikka.asla@gmail.com>
2019-05-13 13:36:10 +0300

    step12

--- puhkirja/src/App.js
+++ puhkirja/src/App.js
@@ -3,6 +3,8 @@ import PersonForm from './components/personForm'
 import Filter from './components/filter'
 import ShowPersons from './components/showPerson'
 import personsServices from './services/persons'
+import './styles/App.css'
+import Notification from './components/notification'
 
 
 const App = () => {
@@ -12,6 +14,10 @@ const App = () => {
   const [ newPhone, setNewPhone] = useState('')
   const [ filter, setFilter] = useState('')
   const [ newFilter, setNewFilter] = useState('')
+  const [notif, setNotif] = useState({
+    message: null,
+    type: null,
+  })
 
   useEffect(()=> {
     personsServices
@@ -32,7 +38,6 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
-    console.log(event.target.value)
   }
 
   const newFilt = (event) => {
@@ -44,13 +49,30 @@ const App = () => {
   const newPerson = (event) => {
     event.preventDefault()
     if(persons.findIndex(person => person.name === newName) > -1){
-        if(window.confirm(`nimellä on ${newName} jo käytössä numero, haluatko korvata sen?`)){
+        if(window.confirm(`nimellä  ${newName} jo käytössä numero, haluatko korvata sen?`)){
           const id = persons.find(person => person.name === newName).id
           const personObject = {
             name:  newName,
             number: newPhone,
           }
-          personsServices.update(id, personObject)
+          personsServices.update(id, personObject).catch(error => {
+            const newNotif = {
+              message: 'päivitys epäonnistui, päivitä sivu uudelleen',
+              type: 'error',
+            }
+            setNotif(newNotif)
+            setTimeout(() => setNotif({...notif, message : null}), 5000)
+          })
+          setPersons(persons.concat(personObject))
+          const notifState = {
+            message: 'päivitys onnistui',
+            type: 'note',
+          }
+            personsServices.getAll().then(persons => {setPersons(persons)})
+            setNotif(notifState)
+            setTimeout(() => {
+						setNotif({...notif, message: null})
+						}, 5000)
         } 
       
     }else{
@@ -59,20 +81,55 @@ const App = () => {
       name: newName,
       number: newPhone,
       }
-    personsServices.create(personObject)
-    console.log(persons)
-    setPersons(persons.concat(personObject))
+      const newNotif = {
+        message: `Henkilö ${personObject.name} lisätty`,
+        type: 'note'
+      }
+      setNotif(newNotif)
+      setTimeout(() => setNotif({...notif, message:null}), 5000)  
+    personsServices.create(personObject).then(personsServices.getAll().then(persons => {setPersons(persons)}))
+    .catch(error => {
+      const newNotif = {
+        message: 'lisääminen epäonnistui',
+        type: 'error',
+      }
+      setNotif(newNotif)
+      setTimeout(() => {
+        setNotif({...notif, message:null})
+      }, 5000)
+    })
     setNewName('')
     setNewPhone('')
+
     }
 }
 
 const deletePerson = (event) => {
-  console.log(persons)
-  if(window.confirm(`poistetaanko`)){
   let id = event.target.value
-    personsServices.deletePerson(id).then(response => {setPersons(persons.filter(person => person.id !== id))}).catch(error=>{'nimi on jo poistettu tietokannasta'})
-  console.log(persons)
+  let deletedPerson = persons.find(person => person.id == id)
+  console.log(deletedPerson)
+
+  if(window.confirm(`poistetaanko`)){
+    personsServices.deletePerson(id).catch(error => {
+      const newNotif = {
+        message: 'poistaminen epäonnistui, päivitä sivu tai yritä uudestaan',
+        type: 'error',
+      }
+      setNotif(newNotif)
+      setTimeout(() => {
+        setNotif({...notif, message:null})
+      }, 5000)
+    })
+    const newPersons = persons.filter(person => person.id != id)
+    setPersons(newPersons)
+    const newNotif = {
+      message: `henkilö  poistettu`,
+      type: 'note',
+    }
+    setNotif(newNotif)
+    setTimeout(() => {setNotif({...notif, message:null})
+    }, 5000)
+
   }
 }
 
@@ -80,6 +137,8 @@ const deletePerson = (event) => {
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+
+      <Notification notif={notif}/>
       <Filter
         submit={newFilt}
         filter={newFilter}
@@ -100,6 +159,7 @@ const deletePerson = (event) => {
         filter={filter}
         buttonHandler={deletePerson}
       />
+      
     </div>
   )
 
---
+++ puhkirja/src/components/notification.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Notification = ({notif}) => {
+    const message = notif.message
+    const type = notif.type
+    if (message === null){
+      return null
+    }
+    const style = {
+          background: 'lightgrey',
+          fontSize: '20px',
+          borderStyle: 'solid',
+          borderRadius: '5px',
+          padding: '10px',
+          marginBottom: '10px'
+    }
+    let notifStyle = null;
+    if (type === 'note'){
+      notifStyle = {...style, color : 'green'}
+    }
+    else{
+      notifStyle = {...style, color: 'red'}
+    }
+  
+    return(
+      <div style ={notifStyle}>
+        {message}
+      </div>
+    )
+  }
+
+  export default Notification
