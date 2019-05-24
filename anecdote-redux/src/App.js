8c594e443595f4aadd3b56a0472095fcdf74d73f
aashem <shemeikka.asla@gmail.com>
2019-05-24 10:30:28 +0300

    step4

---
+++ anecdote-redux/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {createAnecdote, voteAnecdote} from './reducers/anecdoteReducer'
+
+
+const App = (props) => {
+  const anecdotes = props.store.getState()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    props.store.dispatch(
+      voteAnecdote(id)
+    )
+  }
+  const create = (event) =>{
+    event.preventDefault()
+    props.store.dispatch(
+      createAnecdote(event.target.anecdote.value)
+    )
+  }
+
+
+  
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={create}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
