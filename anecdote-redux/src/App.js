1fb64f867aee76cfebb9f9635a0d1513f6381908
aashem <shemeikka.asla@gmail.com>
2019-05-24 10:58:02 +0300

    step6

--- anecdote-redux/src/App.js
+++ anecdote-redux/src/App.js
@@ -1,45 +1,15 @@
 import React from 'react';
-import {createAnecdote, voteAnecdote} from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/anecdoteForm'
+import AnecdoteList from './components/anecdoteList'
 
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    props.store.dispatch(
-      voteAnecdote(id)
-    )
-  }
-  const create = (event) =>{
-    event.preventDefault()
-    props.store.dispatch(
-      createAnecdote(event.target.anecdote.value)
-    )
-  }
-
-
-  
+  const store = props.store
   
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={create}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList store={store}/>
+      <AnecdoteForm store={store}/>
     </div>
   )
 }
---
+++ anecdote-redux/src/components/anecdoteForm.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {createAnecdote} from '../reducers/anecdoteReducer'
+
+
+
+const AnecdoteForm = (props) => {
+
+    const create = (event) =>{
+        event.preventDefault()
+        props.store.dispatch(
+          createAnecdote(event.target.anecdote.value)
+        )
+      }
+    
+
+    return(
+    <div>
+    <h2>create new</h2>
+      <form onSubmit={create}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+    )
+
+}
+
+
+
+export default AnecdoteForm
